fb5a3d77d80a21318f823381b1c23f21ec61b0d2
PbPsycho3 <105768628+PbPsycho3@users.noreply.github.com>
2022-05-27 12:37:24 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1601,14 +1601,14 @@ async def cb_about(bot, update: CallbackQuery):
     text=f"<i><u>Bot's Status</u></i>\n"
     text+=f"\n<b><i>Bot's Uptime:</i></b> <code>{time_formatter(time.time() - start_uptime)}</code>\n"
     text+=f"\n<b><i>Bot Funtion:</i></b> <i>Auto Filter Files</i>\n"
-    text+=f"""\n<b><i>Bot Support:</i></b> <a href="https://t.me/CrazyBotszGrp">@CrazyBotszGrp</a>\n"""
-    text+="""\n<b><i>Source Code:</i></b> <a href="https://github.com/CrazyBotsz/Adv-Filter-Bot-V2">Source</a>"""
+    text+=f"""\n<b><i>Bot Support:</i></b> <a href="https://t.me/MalluHubGP">@CrazyBotszGrp</a>\n"""
+    text+="""\n<b><i>Source Code:</i></b> <a href="https://t.me/MalluHubTG">Source</a>"""
 
     buttons = [
         [
             InlineKeyboardButton
                 (
-                    "My Dev ⚡", url="https://t.me/CYBER_YT1"
+                    "My Dev ⚡", url="https://t.me/Crude_X"
                 ),
                 
             InlineKeyboardButton
@@ -1639,10 +1639,10 @@ async def callback_data(bot, update: CallbackQuery):
 
     if query_data == "start":
         buttons = [[
-            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/CYBER_YT1'),
-            InlineKeyboardButton('Source Code 🧾', url ='https://github.com/M-STER-YT/Adv-Auto-Filter-Bot-V2')
+            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/MalluHubTG'),
+            InlineKeyboardButton('Source Code 🧾', url ='https://t.me/MalluHubTG')
         ],[
-            InlineKeyboardButton('Support 🛠', url='https://t.me/M_SRER_TECH_GROUP')
+            InlineKeyboardButton('Support 🛠', url='https://t.me/MalluHubGP')
         ],[
             InlineKeyboardButton('Help ⚙', callback_data="help")
         ]]
